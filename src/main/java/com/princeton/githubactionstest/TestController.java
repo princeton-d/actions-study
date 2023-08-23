eb3ba5c8decb824c40abd6de9f75fe8da7d37c29
kimdonghyun <ssk0080@gmail.com>
2023-08-23 13:56:10 +0900

    test version 1

--- src/main/java/com/princeton/githubactionstest/TestController.java
+++ src/main/java/com/princeton/githubactionstest/TestController.java
@@ -1,9 +1,16 @@
 package com.princeton.githubactionstest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @GetMapping("/v1")
+    public String testV1() {
+        return "hello test version 1";
+    }
+
 }
